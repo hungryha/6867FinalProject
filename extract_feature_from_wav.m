a4a40b06b6262bead2fba2f9631ae8112ced238c
Qian Long <qlong@mit.edu>
2013-12-02 14:40:31 -0500

    changed default melfcc function settings

--- extract_feature_from_wav.m
+++ extract_feature_from_wav.m
@@ -1,9 +1,13 @@
 % extracts feature vectors from one wav file
 % numcep melfcc, numcep deltas, numcep deltadeltas
 function [features] = extract_feature_from_wav(filepath)
-numcep = 20;
+numcep = 12;
+fbtype = 'mel';
+wintime = 0.032;
+hoptime = 0.016;
+nbands = 26;
 [d,sr] = wavread(filepath);
-[mm,aspc] = melfcc(d, sr, 'maxfreq', 8000, 'numcep', numcep, 'nbands', 22, 'fbtype', 'fcmel', 'dcttype', 1, 'usecmp', 1, 'wintime', 0.032, 'hoptime', 0.016, 'preemph', 0, 'dither', 1);
+[mm,aspc] = melfcc(d, sr, 'maxfreq', 8000, 'numcep', numcep, 'nbands', nbands, 'fbtype', fbtype, 'dcttype', 1, 'usecmp', 1, 'wintime', wintime, 'hoptime', hoptime, 'preemph', 0, 'dither', 1);
 first_deltas = deltas(mm);
 second_deltas = deltas(first_deltas);
 features = [mm;first_deltas;second_deltas];
